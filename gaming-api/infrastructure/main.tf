2c6e16123c9ac99818ab044284d0b85ee45f1280
Aram Manukyan <aram.manukyan92@gmail.com>
2026-05-03 21:01:47 +0400

    Added HM20 with S3 remote backend

--- gaming-api/infrastructure/main.tf
+++ gaming-api/infrastructure/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket       = "project-genesis-tf-state-aram-20260503"
+    key          = "project-genesis/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
